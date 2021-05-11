729e8ebda6f0812990eeebb3f0d4e90375454aa1
Q1sj <q1sj@outlook.com>
2021-05-11 09:44:59 +0800

    优化demo

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # common-export
+基于[easyexcel](https://www.yuque.com/easyexcel) 封装  
+只需关心数据查询方法和导出的表头格式  
+[demo](https://github.com/q1sj/common-export/blob/master/src/test/java/com/qsj/export/ExportTest.java)
+```java
+public class ExportTest {
+    public static void main(String[] args) {
+        List<Export> exportList = Arrays.asList(new UserExport());
+        ExportStatusChangeListener<ExportRecord> changeListener = System.out::println;
+        ExportContext<ExportRecord> exportContext = new ExportContext<>(exportList, 1000, "C:/", changeListener);
+        // 模拟从数据库查询待导出记录
+        List<ExportRecord> exports = Arrays.asList(ExportRecord.mock());
+        exportContext.export(exports);
+
+    }
+}
+
+class ExportRecord extends AbstractExportRecord{
+    public static ExportRecord mock(){
+        ExportRecord exportRecord = new ExportRecord();
+        exportRecord.setFileName("filename");
+        exportRecord.setConditions("xxx");
+        exportRecord.setCode("user");
+        return exportRecord;
+    }
+}
+
+class UserExport implements Export{
+
+    @Override
+    public String getExportCode() {
+        return "user";
+    }
+
+    @Override
+    public List<?> getExportList(String conditions) {
+        // 此处模拟 根据条件查询 返回结果集
+        return Arrays.asList(new User("qsj",18),new User("admin",19));
+    }
+}
+
+class User{
+    @ExcelProperty("用户名")
+    private String username;
+    @ExcelProperty("年龄")
+    private int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+```
--- src/test/java/com/qsj/export/ExportTest.java
+++ src/test/java/com/qsj/export/ExportTest.java
@@ -1,5 +1,7 @@
 package com.qsj.export;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,33 +11,64 @@ import java.util.List;
  */
 public class ExportTest {
     public static void main(String[] args) {
-        List<Export> exportList = Arrays.asList(new ExportImpl());
+        List<Export> exportList = Arrays.asList(new UserExport());
         ExportStatusChangeListener<ExportRecord> changeListener = System.out::println;
         ExportContext<ExportRecord> exportContext = new ExportContext<>(exportList, 1000, "C:/", changeListener);
         // 模拟从数据库查询待导出记录
-        List<ExportRecord> exports = Arrays.asList(new ExportRecord());
+        List<ExportRecord> exports = Arrays.asList(ExportRecord.mock());
         exportContext.export(exports);
 
     }
 }
 
 class ExportRecord extends AbstractExportRecord{
-    public ExportRecord() {
-        setCode("1");
-        setConditions("xxx");
-        setFileName("filename");
+    public static ExportRecord mock(){
+        ExportRecord exportRecord = new ExportRecord();
+        exportRecord.setFileName("filename");
+        exportRecord.setConditions("xxx");
+        exportRecord.setCode("user");
+        return exportRecord;
     }
 }
 
-class ExportImpl implements Export{
+class UserExport implements Export{
 
     @Override
     public String getExportCode() {
-        return "1";
+        return "user";
     }
 
     @Override
     public List<?> getExportList(String conditions) {
-        return Arrays.asList(new ExportRecord());
+        // 此处模拟 根据条件查询 返回结果集
+        return Arrays.asList(new User("qsj",18),new User("admin",19));
+    }
+}
+
+class User{
+    @ExcelProperty("用户名")
+    private String username;
+    @ExcelProperty("年龄")
+    private int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
